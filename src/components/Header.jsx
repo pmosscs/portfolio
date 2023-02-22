6c76e1e5a071b08783921442a8142aa3e101ea11
Peyton Moss <pmoss94@gmail.com>
2023-02-22 01:23:52 -0700

    nav bar x added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
@@ -8,22 +8,39 @@ function Header() {
     color: "black",
   };
 
+  const [headerActive, setHeaderActive] = useState(false);
+
+  const clickBurger = () => {
+    console.log("burger clicked");
+    setHeaderActive(!headerActive);
+  };
+
   return (
     <div className="header">
       <p>Peyton Moss</p>
 
       <nav>
-        <Link style={linkstyle} to="">
-          <p>Home</p>
-        </Link>
-        <div className="header-bars"></div>
-        <Link style={linkstyle} to="/contact">
-          <p>Contact</p>
-        </Link>
-        <div className="header-bars"></div>
-        <Link style={linkstyle} to="/now">
-          <p>Now</p>
-        </Link>
+        <div className={`navlinks ${headerActive ? "burger-active" : null}`}>
+          <Link style={linkstyle} to="">
+            <p>Home</p>
+          </Link>
+          <div className={`header-bars`}></div>
+          <Link style={linkstyle} to="/contact">
+            <p>Contact</p>
+          </Link>
+          <div className={`header-bars`}></div>
+          <Link style={linkstyle} to="/now">
+            <p>Now</p>
+          </Link>
+        </div>
+        <div
+          className={`burger ${headerActive ? "burger-x" : null}`}
+          onClick={clickBurger}
+        >
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
       </nav>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,15 +12,10 @@ function Hero() {
         </div>
         <div className="hero-info">
           <p>
-            {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure */}
-            Thank you for visiting. <br /> I am a software engineer/developer
-            with a love for building and creating. <br /> I enjoy problem
-            solving and learning new technologies and skills to implement into
-            my projects. <br /> Feel free to check out my project demos or reach
-            out to me via the links on my contact page.
+            I love building and creating applications people love to use. <br />{" "}
+            I enjoy problem solving and learning new technologies and skills to
+            implement into my projects. <br /> Feel free to check out my project
+            demos or reach out to me via the links on my contact page.
           </p>
         </div>
       </div>
